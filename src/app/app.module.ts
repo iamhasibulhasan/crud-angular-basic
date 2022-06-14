313f301035e291745b6b934c11a96f2150875934
imhasibulhasan <hashibul218@gmail.com>
2022-06-14 12:27:40 +0600

    add new user modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    UserComponent,
   ],
   imports: [
     BrowserModule,
